00209a66aa7fb2048d406913c8246e9c80d3cd91
Alexander List <alist@listprojects.com>
2013-01-18 23:32:10 -0500

    Initial Commit

---
+++ stitch/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  stitch
+//
+//  Created by Alexander List on 1/18/13.
+//  Copyright (c) 2013 ExoMachina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ stitch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  stitch
+//
+//  Created by Alexander List on 1/18/13.
+//  Copyright (c) 2013 ExoMachina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+	}
+}
